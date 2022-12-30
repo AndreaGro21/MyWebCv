444d69f8f8d424e53873effdd96a2a9061d9bf37
AndreaGro21 <101936713+AndreaGro21@users.noreply.github.com>
2022-12-30 15:30:56 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,34 @@
+
+
+
+
+function showDiv() {
+
+
+    document.getElementById('skills').style.display = "inline";
+    document.getElementById('shadow').style.display = "none";
+    document.getElementById('hiddenName').style.display = "block";
+
+}
+
+function closeDiv() {
+
+
+    document.getElementById('skills').style.display = "none";
+    document.getElementById('shadow').style.display = "inline";
+}
+
+
+
+function linkto() {
+    location.assign("https://andreagro21.github.io/firstwebsite/html.html")
+  }
+
+
+
+
+
+
+
+
+
